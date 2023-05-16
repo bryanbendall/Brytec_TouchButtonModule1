3123966a7c598d6a95dc155800621982ccb2b483
Bryan Bendall <b_bendall@hotmail.com>
2023-05-16 13:12:24 -0400

    Setup touch readings

--- src/TouchButtonModule1.cpp
+++ src/TouchButtonModule1.cpp
@@ -5,8 +5,56 @@
 #include "TouchButtonModule1.h"
 #include <avr/interrupt.h>
 #include <avr/pgmspace.h>
+#include <util/delay.h>
 
 #include "Program.h"
+
+unsigned long timeoutCount = 1000000;
+unsigned long pin4baseline = 0;
+unsigned long pin5baseline = 0;
+static constexpr uint32_t samples = 50;
+
+int32_t CapReadPin4()
+{
+    uint32_t totalCharge = 0;
+
+    for (uint32_t i = 0; i < samples; i++) {
+
+        DDRF |= (1 << PINF6); // Make output
+        PORTF &= ~(1 << PINF6); // Set low
+        _delay_ms(1);
+
+        DDRF &= ~(1 << PINF6); // Make input
+        while (!(PINF & (1 << PINF6)) && (totalCharge < timeoutCount))
+            totalCharge++;
+    }
+
+    if ((totalCharge - pin4baseline) <= 0)
+        return 0;
+    else
+        return (totalCharge - pin4baseline);
+}
+
+int32_t CapReadPin5()
+{
+    uint32_t totalCharge = 0;
+
+    for (uint32_t i = 0; i < samples; i++) {
+
+        DDRD |= (1 << PIND4); // Make output
+        PORTD &= ~(1 << PIND4); // Set low
+        _delay_ms(1);
+
+        DDRD &= ~(1 << PIND4); // Make input
+        while (!(PIND & (1 << PIND4)) && (totalCharge < timeoutCount))
+            totalCharge++;
+    }
+
+    if ((totalCharge - pin5baseline) <= 0)
+        return 0;
+    else
+        return (totalCharge - pin5baseline);
+}
 
 namespace Brytec {
 
@@ -29,6 +77,13 @@ void BrytecBoard::setupBrytecCan(uint32_t mask, uint32_t filter)
 
 void BrytecBoard::setupPin(uint16_t index, IOTypes::Types type)
 {
+    static bool first = true;
+    if (first) {
+        pin4baseline = CapReadPin4();
+        pin5baseline = CapReadPin5();
+        first = false;
+    }
+
     switch (index) {
     case BT_PIN_Touch_1:
         break;
@@ -45,10 +100,16 @@ void BrytecBoard::shutdownAllPins()
 float BrytecBoard::getPinValue(uint16_t index)
 {
     switch (index) {
-    case BT_PIN_Touch_1:
-        return 0.0f;
-    case BT_PIN_Touch_2:
-        return 0.0f;
+    case BT_PIN_Touch_1: {
+        int32_t pin4reading = CapReadPin4();
+        if (pin4reading >= 50)
+            return 1.0f;
+    }
+    case BT_PIN_Touch_2: {
+        int32_t pin5reading = CapReadPin5();
+        if (pin5reading >= 50)
+            return 1.0f;
+    }
     }
 
     return 0.0f;
